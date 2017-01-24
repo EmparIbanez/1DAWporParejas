207a7e8c96f10268012d4b87d6b83a18b8c4c6c2
a0y3770325h <a0y3770325h@s09-pc17>
2017-01-24 11:49:18 +0100

    Habitacion

--- src/llibreria/Llibreria.java
+++ src/llibreria/Llibreria.java
@@ -14,7 +14,8 @@ public class Llibreria {
         /* Añade la información */
         Editorial e1 = new Editorial("RA-MA EDITORIAL",1, 2000);
         Editorial e2 = new Editorial("ANAYA EDITORIAL",1, 2000);
-
+   
+        
         Llibre l1 = new Llibre("978-84-9964-169-0", "Informática y comunicaciones grado superior", 2012, 30, e1);
         Llibre l2 = new Llibre("978-84-9964-169-1", "Informática y comunicaciones grado superior", 2016, 30, e2);
 
@@ -24,7 +25,7 @@ public class Llibreria {
             System.out.println("**************************************************************"); 
 
             System.out.println(l2.toString()); 
-        
+            System.out.println(l2.toString()); 
         
     }
     
